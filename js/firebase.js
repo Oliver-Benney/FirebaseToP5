53b3a11437ceb261a4ee160309930c66fed8ce15
Oliver-Benney <93660106+Oliver-Benney@users.noreply.github.com>
2023-02-16 12:57:36 +0000

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyCdYuzDFMHgPE55h7sf_MDYirjUTNsRt-M",
+  authDomain: "oliver-s-firebasetop5.firebaseapp.com",
+  projectId: "oliver-s-firebasetop5",
+  storageBucket: "oliver-s-firebasetop5.appspot.com",
+  messagingSenderId: "147744968570",
+  appId: "1:147744968570:web:3ec7a3c4a51dbb8a0f50c3",
+  measurementId: "G-BKNVTRKSGR"
+});
+
+var db = firebase.firestore()
